364f0ed84ffce0631cbb5314635737cf8432efbc
feeeeelippee <68142296+feeeeelippee@users.noreply.github.com>
2023-08-31 18:35:56 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     e.preventDefault();
     setVisitorName(image.name);
     const visitorImageName=uuid.v4();
-    fetch(`https://8nv235n6xc.execute-api.ap-southeast-1.amazonaws.com/dev/employees-2-images/${visitorImageName}.jpeg` ,{
+    fetch(`https://s5i8z1hzo9.execute-api.ap-southeast-1.amazonaws.com/dev/employees-2-images/${visitorImageName}.jpeg` ,{
     
     method:'PUT',
       headers:{
@@ -39,7 +39,7 @@ function App() {
   }
 
   async function authenticate(visitorImageName){
-    const requestUrl='https://8nv235n6xc.execute-api.ap-southeast-1.amazonaws.com/dev/employee?'+new URLSearchParams({
+    const requestUrl='https://s5i8z1hzo9.execute-api.ap-southeast-1.amazonaws.com/dev/employee?'+new URLSearchParams({
       objectKey:`${visitorImageName}.jpeg`
     });
     return await fetch(requestUrl,{
